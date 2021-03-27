928069bf7649c0c486f29898daea9223be5f9366
davidgq10 <davidgq10@hotmail.com>
2021-03-27 21:22:48 +0000

    Primer comentario

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [light, setLight] = useState("red");
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container m-2">
+			<div className="bg-dark"></div>
+			<div className="bg-dark">
+				<div className="bg-danger"></div>
+				<div className="bg-warning"></div>
+				<div className="bg-success"></div>
+			</div>
 		</div>
 	);
 }
+
+//   <TrafficLight
+//     onRedClick={() => this.setState({ redOn: !this.state.redOn })}
+//     onYellowClick={() => this.setState({ yellowOn: !this.state.yellowOn })}
+//     onGreenClick={() => this.setState({ greenOn: !this.state.greenOn })}
+//     RedOn={this.state.redOn}
+//     YellowOn={this.state.yellowOn}
+//     GreenOn={this.state.greenOn}
+//   />
