3287dfd29e9176dea0d6b6f6cd3b94b071b85251
davidgq10 <davidgq10@hotmail.com>
2021-04-03 01:13:36 +0000

    primer entrega

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,29 +1,90 @@
 import React, { useState } from "react";
+import { Container, Button, Row, Image, Col } from "react-bootstrap";
+import "/workspace/react-hello/src/styles/home.scss";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+//Se exporta semáforo en el componente Home
 export function Home() {
-	const [light, setLight] = useState("red");
+	// Se define hook para cambio de luz
+	const [light, setLight] = useState(0);
 
+	// Se establece una constante la cual evalúa mediante una función con un switch evalúa el estado de la luz, según estado retorna el semáforo con la luz encendida, el semáforo se define mediante las clases establecidas en el home.scss (por cierto me quedó bonito! :)
+	const handleOnClick = light => {
+		switch (light) {
+			//estado 0 luz roja encendida
+			case 0:
+				return (
+					<div className="Semaforo">
+						<div
+							className="Light redOn"
+							onClick={() => setLight(0)}
+							key={0}></div>
+						<div
+							className="Light yellowOff"
+							onClick={() => setLight(1)}
+							key={1}></div>
+						<div
+							className="Light greenOff"
+							onClick={() => setLight(2)}
+							key={2}></div>
+					</div>
+				);
+			//estado 1 luz amarilla encendida
+			case 1:
+				return (
+					<div className="Semaforo">
+						<div
+							className="Light redOff"
+							onClick={() => setLight(0)}></div>
+						<div
+							className="Light yellowOn"
+							onClick={() => setLight(1)}></div>
+						<div
+							className="Light greenOff"
+							onClick={() => setLight(2)}></div>
+					</div>
+				);
+			//estado 2 luz verde encendida
+			case 2:
+				return (
+					<div className="Semaforo">
+						<div
+							className="Light redOff"
+							onClick={() => setLight(0)}></div>
+						<div
+							className="Light yellowOff"
+							onClick={() => setLight(1)}></div>
+						<div
+							className="Light greenOn"
+							onClick={() => setLight(2)}></div>
+					</div>
+				);
+			default:
+				return (
+					<div className="Semaforo">
+						<div
+							className="Light redOn"
+							onClick={() => setLight(0)}></div>
+						<div
+							className="Light yellowOff"
+							onClick={() => setLight(1)}></div>
+						<div
+							className="Light greenOff"
+							onClick={() => setLight(2)}></div>
+					</div>
+				);
+		}
+	};
+
+	//retorna el componente
 	return (
-		<div className="container m-2">
-			<div className="bg-dark"></div>
-			<div className="bg-dark">
-				<div className="bg-danger"></div>
-				<div className="bg-warning"></div>
-				<div className="bg-success"></div>
-			</div>
-		</div>
+		<Container className="d-print-block">
+			<Row>
+				<div className="Poste"></div>
+			</Row>
+			<Row>{handleOnClick(light)}</Row>
+		</Container>
 	);
 }
-
-//   <TrafficLight
-//     onRedClick={() => this.setState({ redOn: !this.state.redOn })}
-//     onYellowClick={() => this.setState({ yellowOn: !this.state.yellowOn })}
-//     onGreenClick={() => this.setState({ greenOn: !this.state.greenOn })}
-//     RedOn={this.state.redOn}
-//     YellowOn={this.state.yellowOn}
-//     GreenOn={this.state.greenOn}
-//   />
